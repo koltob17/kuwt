feb9f47d57608dd2982624fa23882ecf8baff14e
Tobias <koltob17@htl-kaindorf.at>
2021-05-12 14:32:54 +0200

    initial commit

---
+++ src/main/java/htl/kaindorf/kuwt/KuwtApplication.java
@@ -0,0 +1,13 @@
+package htl.kaindorf.kuwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/htl/kaindorf/kuwt/KuwtApplicationTests.java
@@ -0,0 +1,13 @@
+package htl.kaindorf.kuwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
